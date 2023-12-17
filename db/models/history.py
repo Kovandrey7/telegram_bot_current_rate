eafeb11c11f2586cd9938fa39a6f0ff1cd606a0a
Андрей <kov.andrey7@yandex.ru>
2023-12-17 22:30:41 +0300

    changed type values and user_id

--- db/models/history.py
+++ db/models/history.py
@@ -1,8 +1,7 @@
 from datetime import datetime
-from decimal import Decimal
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Integer, func, ForeignKey, DECIMAL
+from sqlalchemy import Integer, func, ForeignKey, Float, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db.models import Base
@@ -13,8 +12,8 @@ if TYPE_CHECKING:
 
 class History(Base):
     history_id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    date: Mapped[datetime] = mapped_column(server_default=func.now(), default=datetime.utcnow)
-    values: Mapped[Decimal] = mapped_column(DECIMAL, nullable=False)
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.user_id"), nullable=False)
+    date: Mapped[datetime] = mapped_column(server_default=func.now(), default=datetime.now)
+    values: Mapped[float] = mapped_column(Float, nullable=False)
+    user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("user.user_id"), nullable=False)
 
     user: Mapped["User"] = relationship(back_populates="histories")
