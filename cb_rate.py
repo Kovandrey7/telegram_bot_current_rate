b0b32e26e2bdedb0ac4faacf0021c7e02556a376
Андрей <kov.andrey7@yandex.ru>
2023-12-17 22:27:24 +0300

    realized get current usd

---
+++ cb_rate.py
@@ -0,0 +1,10 @@
+import aiohttp
+
+
+async def get_current_usd():
+    url = "https://www.cbr-xml-daily.ru/daily_json.js"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            response = await resp.json(content_type="application/javascript")
+            current_usd = response["Valute"]["USD"]["Value"]
+            return current_usd
