2d6c7758aacf5769eec79bb92383c2e21e384c7f
Андрей <kov.andrey7@yandex.ru>
2023-12-18 00:13:31 +0300

    Update version 2.1

---
+++ main.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from cb_rate import get_current_usd
+from config import BOT_TOKEN
+from db.models.crud import add_usd_in_current_rate
+from handlers import user_handlers, callback_query
+from scheduler.scheduler import update_usd_info_cron, send_message_cron
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=BOT_TOKEN)
+
+dp = Dispatcher()
+dp.include_router(user_handlers.router)
+dp.include_router(callback_query.router)
+
+
+async def main():
+    current_usd = await get_current_usd()
+    await add_usd_in_current_rate(current_usd)
+    task = asyncio.create_task(update_usd_info_cron())
+    task_2 = asyncio.create_task(send_message_cron(bot))
+    await task
+    await task_2
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
