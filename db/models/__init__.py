e952322cbcfe5f632d471cbf371a5221328a0cf1
Андрей <kov.andrey7@yandex.ru>
2023-12-17 22:32:35 +0300

    add CurrentRate in init

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "Base",
     "User",
     "History",
+    "CurrentRate",
 )
 
 from .base import Base
 from .user import User
 from .history import History
+from .current_rate import CurrentRate
