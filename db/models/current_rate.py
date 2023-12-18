c47463632140525144be59fda7c2660845c9c56d
Андрей <kov.andrey7@yandex.ru>
2023-12-18 13:46:13 +0300

    deleted mapped_column in value

--- db/models/current_rate.py
+++ db/models/current_rate.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Float
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from db.models.base import Base
@@ -7,4 +7,4 @@ from db.models.base import Base
 class CurrentRate(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     currency_name: Mapped[str] = mapped_column(String(40), nullable=False)
-    value: Mapped[float] = mapped_column(Float, nullable=False)
+    value: Mapped[float]
